64f617891794f81e9eb3911e4ab45e64f804b3b1
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2025-07-04 01:04:17 +0600

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,10 @@
+{/*
 import React from 'react';
 const Header = () => (
   <header className="bg-primary text-white text-center py-5">
-    <h1>Tools Final Project</h1> 
+   # <h1>Tools Final Project</h1> 
   </header>
 );
 
-export default Header;
+export default Header; 
+*/}
